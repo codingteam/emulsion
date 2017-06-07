aef90d543c81416ac0b28e8fea0dbe322abdaa80
Friedrich von Never <friedrich@fornever.me>
2017-06-07 22:13:24 +0700

    #2: add initial Telegram support

--- Program.fs
+++ Program.fs
@@ -1,9 +1,16 @@
-﻿open System
+﻿module Emulsion.Program
+
+open System
 
 open Ctor.Xmpp
 
+let private startXmpp login password room = new Robot(Console.WriteLine, login, password, room, "хортолёт")
+let private startTelegram token = Telegram.run token
+
 [<EntryPoint>]
-let main [| login; password; room |] =
-    use robot = new Robot(Console.WriteLine, login, password, room, "хортолёт")
-    Console.ReadKey() |> ignore
+let main [| login; password; room; token |] =
+    use xmpp = startXmpp login password room
+    let telegram = startTelegram
+    Async.RunSynchronously <| telegram token
+
     0
