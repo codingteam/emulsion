2d719a30dbaa52e792eb189e657bb6c9516f90d8
Friedrich von Never <friedrich@fornever.me>
2017-08-26 15:59:35 +0700

    Update Funogram

--- Program.fs
+++ Program.fs
@@ -8,9 +8,12 @@ let private startXmpp login password room = new Robot(Console.WriteLine, login, 
 let private startTelegram token = Telegram.run token
 
 [<EntryPoint>]
-let main [| login; password; room; token |] =
-    use xmpp = startXmpp login password room
-    let telegram = startTelegram
-    Async.RunSynchronously <| telegram token
+let main = function
+    | [| login; password; room; token |] ->
+        use xmpp = startXmpp login password room
+        startTelegram token
 
-    0
+        0
+    | _ ->
+        printfn "Arguments: login password room token"
+        0
--- Router.fs
+++ Router.fs
@@ -1,99 +1,95 @@
-namespace Funogram.TestBot
+module Emulsion.Router
+
 open Funogram
+open Funogram.Bot
+open Funogram.Api
 open Funogram.Types
+open FunHttp
 
-type Bot = { Token: string; Me: Funogram.Types.User }
+let processResultWithValue (result: Result<'a, ApiResponseError>) =
+    match result with
+    | Ok v -> Some v
+    | Error e ->
+        printfn "Error: %s" e.Description
+        None
 
-module Router =
-
-    let (|AvailableCommands|_|) (msg: Message) text =
-        if text = "/commands" then
-            Some("")
-        else None
-
-    let inline processError<'a> (result: Result<'a,Types.ApiResponseError>) =
-        match result with
-        | Error e -> printf "Error: %s" e.Description
-        | _ -> ()
-
-    let showAvailableCommands token msg =
-        processError <| Telegram.SendMessage(token, ChatId.Long(msg.Chat.Id), """⭐️Available test commands:
+let defaultText = """⭐️Available test commands:
 /send_message1 - Markdown test
 /send_message2 - HTML test
 /send_message3 - Disable web page preview and notifications
 /send_message4 - Test reply message
 /send_message5 - Test ReplyKeyboardMarkup
 /send_message6 - Test RemoveKeyboardMarkup
-
 /forward_message - Test forward message
 /show_my_photos_sizes - Test getUserProfilePhotos method
-/get_chat_info - Returns id and type of current chat""")
+/get_chat_info - Returns id and type of current chat
+/send_photo - Send example photo"""
 
+let processResult (result: Result<'a, ApiResponseError>) =
+    processResultWithValue result |> ignore
 
-    let sendMessage1 token msg =
-        processError <| Telegram.SendMessage(token, ChatId.Long(msg.Chat.Id), "Test *Markdown*", ParseMode.Markdown)
+let updateArrived (token : string) (ctx : UpdateContext) =
+    let bot data = api token data |> Async.RunSynchronously |> processResult
+    let botResult data = api token data |> Async.RunSynchronously
 
-    let sendMessage2 token msg =
-        processError <| Telegram.SendMessage(token, ChatId.Long(msg.Chat.Id), "Test <b>HTML</b>", ParseMode.HTML)
-
-    let sendMessage3 token msg =
-        processError <| Telegram.SendMessage(token, ChatId.Long(msg.Chat.Id), "@Dolfik! See http://fsharplang.ru - Russian F# Community", disableWebPagePreview = true, disableNotification = true)
-
-    let sendMessage4 token msg =
-        processError <| Telegram.SendMessage(token, ChatId.Long(msg.Chat.Id), "That's message with reply!", replyToMessageId = msg.MessageId)
-
-    let sendMessage5 token msg =
-        let keyboard = (Seq.init 2 (fun x -> Seq.init 2 (fun y -> { Text = y.ToString() + x.ToString(); RequestContact = None; RequestLocation = None })))
-        let markup = {
-            Keyboard = keyboard
-            ResizeKeyboard = None
-            OneTimeKeyboard = None
-            Selective = None
-        }
-        processError <| Telegram.SendMessage(token, ChatId.Long(msg.Chat.Id), "That's keyboard!", replyMarkup = Markup.ReplyKeyboardMarkup(markup))
-
-    let sendMessage6 token msg =
-        processError <| Telegram.SendMessage
-                (token,
-                ChatId.Long(msg.Chat.Id), "Keyboard was removed!",
-                replyMarkup = Markup.ReplyKeyboardRemove({ RemoveKeyboard = true; Selective = None; }))
-
-    let forwardMessage token msg =
-        processError <| Telegram.ForwardMessage(token, ChatId.Long(msg.Chat.Id), ChatId.Long(msg.Chat.Id), msg.MessageId)
-
-    let showMyPhotos token msg =
-        match Telegram.GetUserProfilePhotos(token, msg.Chat.Id |> int) with
-        | Ok x -> processError <| Telegram.SendMessage(
-                                    token,
-                                    ChatId.Long(msg.Chat.Id),
-                                    sprintf "Photos: %s" (x.Photos |> Seq.map (fun f -> f |> Seq.last) |> Seq.map (fun f -> sprintf "%ix%i" f.Width f.Height) |> String.concat ","))
+    let getChatInfo msg =
+        let result = botResult (getChat msg.Chat.Id)
+        match result with
+        | Ok x ->
+            botResult (sendMessage msg.Chat.Id (sprintf "Id: %i, Type: %s" x.Id x.Type))
+            |> processResultWithValue
+            |> ignore
         | Error e -> printf "Error: %s" e.Description
 
-    let getChatInfo token msg =
-        match Telegram.GetChat(token, ChatId.Long(msg.Chat.Id)) with
-        | Ok x -> processError <| Telegram.SendMessage(
-                                    token,
-                                    ChatId.Long(msg.Chat.Id),
-                                    sprintf "Id: %i, Type: %s" x.Id x.Type)
-        | Error e -> printf "Error: %s" e.Description
+    let sendPhoto msg =
+        let image = Http.RequestStream("https://upload.wikimedia.org/wikipedia/commons/f/f5/Example_image.jpg")
+        bot (sendPhoto msg.Chat.Id (FileToSend.File("example.jpg", image.ResponseStream)) "Example")
 
-    let processMessage (bot: Bot) (message: Message) =
-        match message.Text with
-        | Some x ->
-            match x with
-            | "/send_message1" -> sendMessage1 bot.Token message
-            | "/send_message2" -> sendMessage2 bot.Token message
-            | "/send_message3" -> sendMessage3 bot.Token message
-            | "/send_message4" -> sendMessage4 bot.Token message
-            | "/send_message5" -> sendMessage5 bot.Token message
-            | "/send_message6" -> sendMessage6 bot.Token message
-            | "/forward_message" -> forwardMessage bot.Token message
-            | "/show_my_photos_sizes" -> showMyPhotos bot.Token message
-            | "/get_chat_info" -> getChatInfo bot.Token message
-            | _ -> showAvailableCommands bot.Token message
-        | _ -> ()
+    let fromId() = ctx.Update.Message.Value.From.Value.Id
 
-    let updateArrived (bot: Bot) (update: Update) =
-        match update with
-            | UpdateType.Message msg -> processMessage bot msg
-            | _ -> ()
+    let sayWithArgs text parseMode disableWebPagePreview disableNotification replyToMessageId replyMarkup =
+        bot (sendMessageBase (ChatId.Int (fromId())) text parseMode disableWebPagePreview disableNotification replyToMessageId replyMarkup)
+
+    let sendMessageFormatted text parseMode = bot (sendMessageBase (ChatId.Int(fromId())) text (Some parseMode) None None None None)
+
+    let result =
+        processCommands ctx [
+            cmd "/send_message1" (fun _ -> sendMessageFormatted "Test *Markdown*" ParseMode.Markdown)
+            cmd "/send_message2" (fun _ -> sendMessageFormatted "Test <b>HTML</b>" ParseMode.HTML)
+            cmd "/send_message3" (fun _ -> sayWithArgs "@Dolfik! See http://fsharplang.ru - Russian F# Community" None (Some true) (Some true) None None)
+            cmd "/send_message4" (fun _ -> sayWithArgs "That's message with reply!" None None None (Some ctx.Update.Message.Value.MessageId) None)
+            cmd "/send_message5" (fun _ ->
+            (
+                let keyboard = (Seq.init 2 (fun x -> Seq.init 2 (fun y -> { Text = y.ToString() + x.ToString(); RequestContact = None; RequestLocation = None })))
+                let markup = Markup.ReplyKeyboardMarkup {
+                    Keyboard = keyboard
+                    ResizeKeyboard = None
+                    OneTimeKeyboard = None
+                    Selective = None
+                }
+                bot (sendMessageMarkup (fromId()) "That's keyboard!" markup)
+            ))
+            cmd "/send_message6" (fun _ ->
+            (
+                let markup = Markup.ReplyKeyboardRemove { RemoveKeyboard = true; Selective = None; }
+                bot (sendMessageMarkup (fromId()) "Keyboard was removed!" markup)
+            ))
+            cmd "/forward_message" (fun _ -> bot (forwardMessage (fromId()) (fromId()) ctx.Update.Message.Value.MessageId))
+            cmd "/show_my_photos_sizes" (fun _ ->
+            (
+                let x = botResult (getUserProfilePhotosAll (fromId())) |> processResultWithValue
+                if x.IsNone then ()
+                else
+                    let text = sprintf "Photos: %s" (x.Value.Photos
+                                |> Seq.map (fun f -> f |> Seq.last)
+                                |> Seq.map (fun f -> sprintf "%ix%i" f.Width f.Height)
+                                |> String.concat ",")
+
+                    bot (sendMessage (fromId()) text)
+            ))
+            cmd "/get_chat_info" (fun _ -> getChatInfo ctx.Update.Message.Value)
+            cmd "/send_photo" (fun _ -> sendPhoto ctx.Update.Message.Value)
+        ]
+
+    if result then ()
+    else bot (sendMessage (fromId()) defaultText)
