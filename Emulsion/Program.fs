b832c7272ae9e8dd86092273ac992879ceeece62
Friedrich von Never <friedrich@fornever.me>
2017-10-18 23:05:17 +0700

    Use Akka.NET to manage the actors

--- Emulsion/Program.fs
+++ Emulsion/Program.fs
@@ -3,13 +3,21 @@
 open System
 open System.IO
 
+open Akka.FSharp
 open Microsoft.Extensions.Configuration
 
 open Emulsion.Settings
 open Emulsion.Xmpp
 
-let private startXmpp settings = new Robot(Console.WriteLine, settings)
-let private startTelegram = Telegram.run
+let private startXmpp settings = async {
+    printfn "Starting XMPP"
+    ignore (new Robot(Console.WriteLine, settings))
+}
+
+let private startTelegram settings = async {
+    printfn "Starting Telegram"
+    Telegram.run settings
+}
 
 let private getConfiguration directory fileName =
     let config =
@@ -19,9 +27,24 @@ let private getConfiguration directory fileName =
             .Build()
     Settings.read config
 
+let private xmppActor system config =
+    let robot = startXmpp config |> Async.Start
+    actorOf (fun msg -> ())
+
+let private telegramActor system config =
+    let robot = startTelegram config |> Async.Start
+    actorOf (fun msg -> ())
+
 let private startApp config =
-    use xmpp = startXmpp config.xmpp
-    startTelegram config.telegram
+    async {
+        use system = System.create "emulsion" (Configuration.defaultConfig())
+        let xmpp = spawn system "xmpp" (xmppActor system config.xmpp)
+        let telegram = spawn system "telegram" (telegramActor system config.telegram)
+        do! Async.AwaitTask system.WhenTerminated
+    }
+
+let private runApp app =
+    Async.RunSynchronously app
     0
 
 let private defaultConfigFileName = "emulsion.json"
@@ -31,9 +54,11 @@ let main = function
     | [| |] ->
         getConfiguration (Directory.GetCurrentDirectory()) defaultConfigFileName
         |> startApp
+        |> runApp
     | [| configPath |] ->
         getConfiguration (Path.GetDirectoryName configPath) (Path.GetFileName configPath)
         |> startApp
+        |> runApp
     | _ ->
         printfn "Arguments: [config file name] (%s by default)" defaultConfigFileName
         0
