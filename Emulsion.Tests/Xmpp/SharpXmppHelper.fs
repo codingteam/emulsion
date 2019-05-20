ec50a5e334f13c11c3041cb42e4c07c70d78e7d5
Friedrich von Never <friedrich@fornever.me>
2019-05-20 23:50:06 +0700

    Add check for isOwnMessage into the XMPP messenger (#22)

--- Emulsion.Tests/Xmpp/SharpXmppHelper.fs
+++ Emulsion.Tests/Xmpp/SharpXmppHelper.fs
@@ -9,11 +9,15 @@ open Xunit
 open Emulsion
 open Emulsion.Xmpp
 
+let private createXmppMessage (senderJid : string) (text : string) =
+    let element = XMPPMessage(Text = text)
+    element.SetAttributeValue(XName.Get "from", senderJid)
+    element
+
 [<Fact>]
 let ``parseMessage should extract message text and author``() =
     let text = "text test"
-    let element = XMPPMessage(Text = text)
-    element.SetAttributeValue(XName.Get "from", "x@y/author")
+    let element = createXmppMessage "x@y/author" text
     let message = SharpXmppHelper.parseMessage element
     let expected = Some(XmppMessage("author", text))
     Assert.Equal(expected, message)
@@ -23,3 +27,18 @@ let ``Message without author parses to None``() =
     let element = XMPPMessage(Text = "xxx")
     let message = SharpXmppHelper.parseMessage element
     Assert.Equal(None, message)
+
+[<Fact>]
+let ``isOwnMessage detects own message by resource``() =
+    let message = createXmppMessage "a@b/myNickname" "text"
+    Assert.True(SharpXmppHelper.isOwnMessage "myNickname" message)
+
+[<Fact>]
+let ``isOwnMessage detects foreign message``() =
+    let message = createXmppMessage "a@b/notMyNickname" "text"
+    Assert.False(SharpXmppHelper.isOwnMessage "myNickname" message)
+
+[<Fact>]
+let ``isOwnMessage detects nobody's message``() =
+    let message = XMPPMessage() // no "from" attribute
+    Assert.False(SharpXmppHelper.isOwnMessage "myNickname" message)
