2fa005794771280c5f5c93fbe8941599a5770943
Friedrich von Never <friedrich@fornever.me>
2019-05-25 00:20:14 +0700

    Cache the elements and namespaces in SharpXmppHelper

--- Emulsion/Xmpp/SharpXmppHelper.fs
+++ Emulsion/Xmpp/SharpXmppHelper.fs
@@ -9,10 +9,24 @@ open SharpXMPP.XMPP.Client.Elements
 
 open Emulsion
 
+module Namespaces =
+    let StorageBookmarks = XNamespace.Get("storage:bookmarks")
+
+module Elements =
+    let Body = XName.Get "body"
+    let Delay = XName.Get "delay"
+    let From = XName.Get "from"
+    let Jid = XName.Get "jid"
+    let Type = XName.Get "type"
+    let To = XName.Get "to"
+
+open Namespaces
+open Elements
+
 let private bookmark (roomJid: string) (nickname: string): BookmarkedConference =
     let room = BookmarkedConference()
-    room.SetAttributeValue(XName.Get("jid"), roomJid)
-    let nickElement = XElement(XNamespace.Get("storage:bookmarks") + "nick", Value = nickname)
+    room.SetAttributeValue(Jid, roomJid)
+    let nickElement = XElement(StorageBookmarks + "nick", Value = nickname)
     room.Add(nickElement)
     room
 
@@ -22,15 +36,15 @@ let joinRoom (client: XmppClient) (roomJid: string) (nickname: string): unit =
 
 let message (toAddr : string) (text : string) =
     let m = XMPPMessage()
-    m.SetAttributeValue(XName.Get "type", "groupchat")
-    m.SetAttributeValue(XName.Get "to", toAddr)
-    let body = XElement(XName.Get "body")
+    m.SetAttributeValue(Type, "groupchat")
+    m.SetAttributeValue(To, toAddr)
+    let body = XElement(Body)
     body.Value <- text
     m.Add(body)
     m
 
 let private getAttributeValue (element : XElement) attributeName =
-    let attribute = element.Attribute(XName.Get attributeName)
+    let attribute = element.Attribute(attributeName)
     if isNull attribute
     then None
     else Some attribute.Value
@@ -38,14 +52,14 @@ let private getAttributeValue (element : XElement) attributeName =
 let private getResource jidText = JID(jidText).Resource
 
 let isOwnMessage (nickname : string) (message : XMPPMessage) : bool =
-    getAttributeValue message "from"
+    getAttributeValue message From
     |> Option.map getResource
     |> Option.map(fun resource -> resource = nickname)
     |> Option.defaultValue false
 
 let parseMessage (message : XMPPMessage) : IncomingMessage =
     let nickname =
-        getAttributeValue message "from"
+        getAttributeValue message From
         |> Option.map getResource
         |> Option.defaultValue "[UNKNOWN USER]"
     XmppMessage { author = nickname; text = message.Text }
