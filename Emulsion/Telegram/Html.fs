0fdeecb5cdea0d7e5b97d8efcb933968223c0219
Friedrich von Never <friedrich@fornever.me>
2019-05-25 17:41:41 +0700

    Reimplement Html.escape via String.collect (#21)

--- Emulsion/Telegram/Html.fs
+++ Emulsion/Telegram/Html.fs
@@ -1,14 +1,9 @@
 module Emulsion.Telegram.Html
-open System.Text
 
 /// HTML escaping for Telegram only. According to the docs: https://core.telegram.org/bots/api#html-style
-let escape (text : string) : string =
-    let result = StringBuilder text.Length
-    for char in text do
-        ignore <|
-            match char with
-            | '<' -> result.Append "&lt;"
-            | '>' -> result.Append "&gt;"
-            | '&' -> result.Append "&amp;"
-            | other -> result.Append other
-    result.ToString()
+let escape : string -> string =
+    String.collect(function
+                   | '<' -> "&lt;"
+                   | '>' -> "&gt;"
+                   | '&' -> "&amp;"
+                   | other -> other.ToString())
