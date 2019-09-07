1afff5a470a54a8cebb5ce86d369d68c9f61e14a
Friedrich von Never <friedrich@fornever.me>
2019-09-07 19:12:53 +0700

    Add tests for the enter function (#18)

--- Emulsion.Tests/Xmpp/XmppClientFactory.fs
+++ Emulsion.Tests/Xmpp/XmppClientFactory.fs
@@ -3,10 +3,14 @@ namespace Emulsion.Tests.Xmpp
 open Emulsion.Xmpp.XmppClient
 
 type XmppClientFactory =
-    static member create(?connect, ?addConnectionFailedHandler): IXmppClient =
+    static member create(?connect, ?addConnectionFailedHandler, ?addPresenceHandler, ?joinMultiUserChat): IXmppClient =
         let connect = defaultArg connect <| fun () -> async { return () }
         let addConnectionFailedHandler = defaultArg addConnectionFailedHandler <| fun _ _ -> ()
+        let addPresenceHandler = defaultArg addPresenceHandler <| fun _ _ -> ()
+        let joinMultiUserChat = defaultArg joinMultiUserChat <| fun _ _ -> ()
         { new IXmppClient with
             member __.Connect() = connect()
             member __.AddConnectionFailedHandler lt handler = addConnectionFailedHandler lt handler
+            member __.AddPresenceHandler lt handler = addPresenceHandler lt handler
+            member __.JoinMultiUserChat roomJid nickname = joinMultiUserChat roomJid nickname
         }
