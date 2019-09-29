52f534c5cb2eff00f18ff438eb6f2756d7bc6bd5
Friedrich von Never <friedrich@fornever.me>
2019-09-29 22:27:03 +0700

    Deduplicate the MessageSender code (#82)

--- Emulsion/MessageSender.fs
+++ Emulsion/MessageSender.fs
@@ -55,54 +55,38 @@ let private receiver ctx (inbox: Sender) =
     let rec loop (state: State) = async {
         ctx.Logger.Debug("Current queue state: {State}", state)
 
+        let processIncomingMessage msg = async {
+            let newState =
+                match msg with
+                | QueueMessage m ->
+                    let newMessages = Queue.conj m state.Messages
+                    { state with Messages = newMessages }
+                | SetReceiveStatus status ->
+                    { state with ReadyToAcceptMessages = status }
+            return! processState ctx newState
+        }
+
+        let blockAndProcessIncomingMessage() = async {
+            let! msg = inbox.Receive()
+            return! processIncomingMessage msg
+        }
+
         let! nextState =
             match state.ReadyToAcceptMessages, state.Messages with
             | false, _ -> // We aren't permitted to send any messages, we have nothing other to do than block on the
-                          // queue.
-                async {
-                    let! msg = inbox.Receive()
-                    let newState =
-                        match msg with
-                        | QueueMessage m ->
-                            let newMessages = Queue.conj m state.Messages
-                            { state with Messages = newMessages }
-                        | SetReceiveStatus status ->
-                            { state with ReadyToAcceptMessages = status }
-                    return! processState ctx newState
-                }
+                          // message queue.
+                blockAndProcessIncomingMessage()
             | true, Queue.Cons _ -> // We're permitted to send a message and the queue is not empty.
                 // Peek the queued message: maybe it's the `SetReceiveStatus false` which should have the priority over
-                // everything else.
+                // everything else. If there're no incoming messages, then just process the current state as usual.
                 async {
                     match! inbox.TryReceive 0 with
-                    | Some msg ->
-                        return! async {
-                            let newState =
-                                match msg with
-                                | QueueMessage m ->
-                                    let newMessages = Queue.conj m state.Messages
-                                    { state with Messages = newMessages }
-                                | SetReceiveStatus status ->
-                                    { state with ReadyToAcceptMessages = status }
-                            return! processState ctx newState
-                        }
-                    | None ->
-                        return! processState ctx state
+                    | Some msg -> return! processIncomingMessage msg
+                    | None -> return! processState ctx state
                 }
-
             | true, Queue.Nil -> // We're allowed to send a message, but the queue is empty. We have nothing to send,
-                                 // thus we have nothing to do other than to block.
-                async {
-                    let! msg = inbox.Receive()
-                    let newState =
-                        match msg with
-                        | QueueMessage m ->
-                            let newMessages = Queue.conj m state.Messages
-                            { state with Messages = newMessages }
-                        | SetReceiveStatus status ->
-                            { state with ReadyToAcceptMessages = status }
-                    return! processState ctx newState
-                }
+                                 // thus we have nothing to do other than to block on the message queue.
+                blockAndProcessIncomingMessage()
 
         return! loop nextState
     }
