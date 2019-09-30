aae8ff30d0d78edb500a868b6be6ee6bfa507991
Friedrich von Never <friedrich@fornever.me>
2019-09-30 23:13:21 +0700

    Spell out the types

--- Emulsion.Tests/TestUtils/Waiter.fs
+++ Emulsion.Tests/TestUtils/Waiter.fs
@@ -6,8 +6,8 @@ open System.Threading
 let defaultTimeout = TimeSpan.FromSeconds 30.0
 let shortTimeout = TimeSpan.FromSeconds 1.0
 
-let waitForItemCountCond (buffer: LockedBuffer<_>) (condition: int -> bool) (timeout: TimeSpan) =
+let waitForItemCountCond (buffer: LockedBuffer<_>) (condition: int -> bool) (timeout: TimeSpan): bool =
     SpinWait.SpinUntil((fun () -> condition(buffer.Count())), timeout)
 
-let waitForItemCount (buffer: LockedBuffer<_>) count (timeout: TimeSpan) =
+let waitForItemCount (buffer: LockedBuffer<_>) count (timeout: TimeSpan): bool =
     waitForItemCountCond buffer (fun c -> c = count) timeout
