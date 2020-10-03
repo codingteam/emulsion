eb81f7129e50a254b521841449ada995dd039144
Friedrich von Never <friedrich@fornever.me>
2020-10-03 19:18:22 +0700

    #88: add tests for ping behavior

--- Emulsion.Tests/Xmpp/XmppClientFactory.fs
+++ Emulsion.Tests/Xmpp/XmppClientFactory.fs
@@ -7,12 +7,14 @@ type XmppClientFactory =
                          ?joinMultiUserChat,
                          ?send,
                          ?addConnectionFailedHandler,
+                         ?addIqHandler,
                          ?addPresenceHandler,
                          ?addMessageHandler): IXmppClient =
         let connect = defaultArg connect <| fun () -> async { return () }
         let joinMultiUserChat = defaultArg joinMultiUserChat <| fun _ _ _ -> ()
         let send = defaultArg send ignore
         let addConnectionFailedHandler = defaultArg addConnectionFailedHandler <| fun _ _ -> ()
+        let addIqHandler = defaultArg addIqHandler <| fun _ _ -> ()
         let addPresenceHandler = defaultArg addPresenceHandler <| fun _ _ -> ()
         let addMessageHandler = defaultArg addMessageHandler <| fun _ _ -> ()
         { new IXmppClient with
@@ -22,7 +24,7 @@ type XmppClientFactory =
             member _.AddConnectionFailedHandler lt handler = addConnectionFailedHandler lt handler
             member _.AddSignedInHandler _ _ = ()
             member _.AddElementHandler _ _ = ()
-            member _.AddIqHandler _ _ = ()
+            member _.AddIqHandler lt handler = addIqHandler lt handler
             member _.AddPresenceHandler lt handler = addPresenceHandler lt handler
             member _.AddMessageHandler lt handler = addMessageHandler lt handler
         }
