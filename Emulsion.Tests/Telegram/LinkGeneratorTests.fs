2e063dc47174964b9b841fb01dfc6e079b50a8c7
Friedrich von Never <friedrich@fornever.me>
2021-10-03 23:40:58 +0700

    (#102) LinkGeneratorTests: test basic generation

---
+++ Emulsion.Tests/Telegram/LinkGeneratorTests.fs
@@ -0,0 +1,32 @@
+﻿module Emulsion.Tests.Telegram.LinkGeneratorTests
+
+open Emulsion.Telegram
+open Funogram.Telegram.Types
+open Xunit
+
+let private chatName = "test_chat"
+let private messageTemplate =
+    { defaultMessage with
+        Chat =
+            { defaultChat with
+                Type = SuperGroup
+                Username = Some chatName
+            }
+    }
+
+let private doBasicLinkTest message =
+    let links = Async.RunSynchronously(LinkGenerator.gatherLinks message)
+    Assert.Equal(Some $"https://t.me/{chatName}/{message.MessageId}", links.ContentLink)
+
+[<Fact>]
+let documentLinkTest(): unit =
+    doBasicLinkTest
+        { messageTemplate with
+            Document = Some {
+                FileId = ""
+                Thumb = None
+                FileName = None
+                MimeType = None
+                FileSize = None
+            }
+        }
