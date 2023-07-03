729e5a21f3b96b58352502cf2ce8986a08dc0563
Friedrich von Never <friedrich@fornever.me>
2023-07-03 23:37:55 +0200

    (#138) MessageArchive: request the count on the frontend

---
+++ Emulsion.MessageArchive.Frontend/api.d.ts
@@ -0,0 +1,3 @@
+﻿type Statistics = {
+    messageCount: number;
+}
--- Emulsion.MessageArchive.Frontend/app.tsx
+++ Emulsion.MessageArchive.Frontend/app.tsx
@@ -1,4 +1,39 @@
-﻿import React from 'react';
+﻿import React, {useState} from 'react';
 import {render} from 'react-dom';
 
-render(<h1>Hello, world!</h1>, document.getElementById('app'));
+class LoadedPage {
+    constructor(public readonly statistics: Statistics) {}
+}
+
+class ErrorState {
+    constructor(public readonly error: string) {}
+}
+
+type State = 'Loading' | LoadedPage | ErrorState;
+
+const getStatistics = (): Promise<Statistics> => {
+    let url = window.location.href
+    url = url.substring(0, url.lastIndexOf('/'));
+
+    return fetch(`${url}/api/history/statistics`)
+        .then(response => response.json())
+};
+
+const App = () => {
+    const [state, setState] = useState<State>('Loading');
+    if (state === 'Loading') {
+        getStatistics()
+            .then(statistics => setState(new LoadedPage(statistics)))
+            .catch(error => setState(new ErrorState(error.message)));
+    }
+
+    if (state === 'Loading') {
+        return <div className="loading">Loading…</div>
+    } else if (state instanceof ErrorState) {
+        return <div className="error">Error: {state.error}</div>
+    } else {
+        return <div className="page">Count: {state.statistics.messageCount}</div>
+    }
+};
+
+render(<App/>, document.getElementById('app'));
