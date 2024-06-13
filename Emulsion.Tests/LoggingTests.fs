ac28b9fc8eaf4b4fed7355d4dcecebba5c0557a2
Friedrich von Never <friedrich@fornever.me>
2024-06-14 00:52:53 +0200

    Tests: fix possible race conditions on list update

--- Emulsion.Tests/LoggingTests.fs
+++ Emulsion.Tests/LoggingTests.fs
@@ -12,18 +12,20 @@ let ``attachToRdLogSystem should proxy the logged messages``(): unit =
     let events = ResizeArray()
     let serilogLogger = {
         new ILogger with
-            override this.Write(logEvent) = events.Add logEvent
+            override this.Write(logEvent) = lock events (fun() -> events.Add logEvent)
     }
     let rdLogger = Log.GetLog "LoggingTests"
     use _ = Logging.attachToRdLogSystem serilogLogger
     rdLogger.Info "foo"
-    let event = Assert.Single events
+    let event = lock events (fun() -> Assert.Single events)
     Assert.Equal(LogEventLevel.Information, event.Level)
     Assert.Equal("foo", event.MessageTemplate.Text)
     Assert.Equal("\"LoggingTests\"", event.Properties[Constants.SourceContextPropertyName].ToString())
-    events.Clear()
+    lock events (fun() ->
+        events.Clear()
 
-    rdLogger.Error("Test {0}", 1)
-    let event = Assert.Single events
-    Assert.Equal(LogEventLevel.Error, event.Level)
-    Assert.Equal("Test 1", event.MessageTemplate.Text)
+        rdLogger.Error("Test {0}", 1)
+        let event = Assert.Single events
+        Assert.Equal(LogEventLevel.Error, event.Level)
+        Assert.Equal("Test 1", event.MessageTemplate.Text)
+    )
