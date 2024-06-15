2f97e6e50206cb0f54f2ceb5c2cf6a9ea31f4e03
Friedrich von Never <friedrich@fornever.me>
2024-06-15 20:55:09 +0200

    Actors: drop the Akka actors

--- Emulsion/Actors/Factories.fs
+++
@@ -1,9 +0,0 @@
-[<AutoOpen>]
-module Emulsion.Actors.Factories
-
-open Akka.Actor
-
-type ActorFactory = IActorRefFactory -> string -> IActorRef
-type ActorFactories =
-    { xmppFactory : ActorFactory
-      telegramFactory : ActorFactory }
